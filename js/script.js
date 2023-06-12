91994da33b01229f591abeaab52a88dd768b8c63
Zhasulan Muratov <136080283+Zhasulan711@users.noreply.github.com>
2023-06-12 18:03:16 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ const quotes = [
     source: "Walter Isaacson",
     citation: "'Steve Jobs'",
     year: 2011,
+    tags: ["inspiration", "work", "passion"]
   },
   {
     quote:
@@ -23,24 +24,28 @@ const quotes = [
     source: "Nelson Mandela",
     citation: "speech",
     year: 1994,
+    tags: ["inspiration", "perseverance"]
   },
   {
     quote: "Imagination is more important than knowledge.",
     source: "Alice Calaprice",
     citation: "'The Ultimate Quotable Einstein'",
     year: 1940,
+    tags: ["imagination", "knowledge"]
   },
   {
     quote: "You may encounter many defeats, but you must not be defeated.",
     source: "Maya Angelou",
     citation: "'I Know Why the Caged Bird Sings'",
     year: 1969,
+    tags: ["resilience", "self-empowerment"]
   },
   {
     quote: "Nothing in life is to be feared; it is only to be understood.",
     source: "Eve Curie",
     citation: "'Madame Curie: A Biography'",
     year: 1937,
+    tags: ["fear", "understanding"]
   },
 ];
 
@@ -54,8 +59,8 @@ function Randomcolor() {
 /***
  * `color` function for background color!
  ***/
-function color(value) {
-  const randomRGB = `rgb(${value}, ${value}, ${value})`;
+function color() {
+  const randomRGB = `rgb(${Randomcolor()}, ${Randomcolor()}, ${Randomcolor()})`;
   return randomRGB;
 }
 
@@ -79,6 +84,9 @@ const printQuote = function (quoteObject) {
 
   if (quoteObject.year) {
     html += `<span class="year">${quoteObject.year}</span>`;
+  }
+  if (quoteObject.tags) {
+    html += `<br><span class="tags"> ${quoteObject.tags.join(", ")}</span>`;
   }
   html += `</p>`;
   // set bacground color, i find information in this link https://stackoverflow.com/questions/197748/how-do-i-change-the-background-color-with-javascript
@@ -106,6 +114,7 @@ const intervalID = setInterval(function () {
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
+// i find information about regular intervals in this links https://developer.mozilla.org/en-US/docs/Web/API/setInterval and https://doka.guide/js/setinterval/
 document.getElementById("load-quote").addEventListener(
   "click",
   function () {
